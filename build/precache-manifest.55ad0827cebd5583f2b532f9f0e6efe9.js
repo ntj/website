05cccc3cfe7f43a469331bd47b3eef071fdd55a6
ntj <ntj@allesjetzt.net>
2020-10-16 23:06:57 +0200

    Changes

---
+++ build/precache-manifest.55ad0827cebd5583f2b532f9f0e6efe9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430859b0439a46720dc95a50e47681d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b0426e72de4ecee552a",
+    "url": "/static/css/2.ddea7309.chunk.css"
+  },
+  {
+    "revision": "f1481495246ca96d742a",
+    "url": "/static/css/main.9ad5faf6.chunk.css"
+  },
+  {
+    "revision": "2b0426e72de4ecee552a",
+    "url": "/static/js/2.3d7625c1.chunk.js"
+  },
+  {
+    "revision": "13d2b5e83e00efe1c971c289a2ed6e5c",
+    "url": "/static/js/2.3d7625c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1481495246ca96d742a",
+    "url": "/static/js/main.6a96f517.chunk.js"
+  },
+  {
+    "revision": "0e258aef7a3291f3abc2",
+    "url": "/static/js/runtime-main.8b42af49.js"
+  }
+]);
